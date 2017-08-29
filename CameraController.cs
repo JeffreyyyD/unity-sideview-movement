29f7bae3dcf6c1708c0a4742ff70ac27c02c14a2
Jeffrey Davis <31411434+JeffreyyyD@users.noreply.github.com>
2017-08-28 22:24:10 -0500

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private float moveSpeed = 3f;
+
+    private bool followPlayer = true;
+
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 offset;        
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        } else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveUp();
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            followPlayer = !followPlayer;
+        }
+
+        if(followPlayer)
+        {
+            FollowPlayer();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return) && !followPlayer)
+        {
+            SnapToPlayer();
+        }
+    }
+
+    private void SnapToPlayer()
+    {
+        transform.position = player.transform.position + offset;
+
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 velocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref velocity, 0.05f);
+    }
+
+    private void MoveRight()
+    {
+        this.gameObject.transform.Translate(Vector3.right * Time.deltaTime * 3f);
+    }
+
+    private void MoveLeft()
+    {
+        this.gameObject.transform.Translate(Vector3.left * Time.deltaTime * 3f);
+    }
+
+    private void MoveUp()
+    {
+        this.gameObject.transform.Translate(Vector3.up * Time.deltaTime * 3f);
+    }
+
+    private void MoveDown()
+    {
+        this.gameObject.transform.Translate(Vector3.down * Time.deltaTime * 3f);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed = 3f;
+    [SerializeField]
+    private float jumpForce = 3f;
+    [SerializeField]
+    private bool canJump = true;
+    [SerializeField]
+    private bool isGrounded = true;
+    private Animator anim;
+
+
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        anim = this.gameObject.GetComponent<Animator>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            MoveForward();
+            anim.SetBool("Walking", true);
+            anim.SetBool("Idle", false);
+
+        } else if(Input.GetKey(KeyCode.A))
+        {
+            MoveBackward();
+            anim.SetBool("Walking", true);
+            anim.SetBool("Idle", false);
+        } else
+        {
+            anim.SetBool("Walking", false);
+            anim.SetBool("Idle", true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && canJump)
+        {
+            canJump = false;
+            anim.SetTrigger("Jump");
+            Jump();
+        }
+
+    }
+
+    private void MoveForward()
+    {
+        Vector3 forwardVelocity = speed * Vector3.right;
+
+        rb.MovePosition(rb.position + forwardVelocity * Time.deltaTime);
+    }
+
+    private void MoveBackward()
+    {
+        Vector3 backwardVelocity = speed * Vector3.left;
+
+        rb.MovePosition(rb.position + backwardVelocity * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        Vector3 jumpVelocity = jumpForce * Vector3.up;
+
+        rb.AddForce(jumpVelocity);
+
+        StartCoroutine(CanJump());
+    }
+
+    private IEnumerator CanJump()
+    {
+        yield return new WaitForSeconds(.25f);
+
+        canJump = true;
+    }
+
+    #region Ground detection for jumps
+    void OnCollisionStay(Collision collider)
+    {
+        if (collider.gameObject.tag == "Walkable")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collider)
+    {
+        if(collider.gameObject.tag == "Walkable")
+        {
+            isGrounded = false;
+        }
+    }
+    #endregion
+}
